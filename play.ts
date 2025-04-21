3d27853e276c1b38b2b5b14b014f06fdf0091d0b
Colin McDonnell <colinmcd94@gmail.com>
2025-04-21 16:12:35 -0700

    WIP

--- play.ts
+++ play.ts
@@ -1,3 +1,24 @@
 import * as z from "zod";
 
 z;
+const A = z.interface({
+  name: z.string(),
+  get b() {
+    return B;
+  },
+});
+// .readonly();
+// .meta({ id: "A" });
+
+const B = z
+  .interface({
+    name: z.string(),
+    get a() {
+      return A;
+    },
+  })
+  .readonly();
+// .meta({ id: "B" });
+
+// const result = z.toJSONSchema(A);
+// console.dir(result, { depth: 10 });
